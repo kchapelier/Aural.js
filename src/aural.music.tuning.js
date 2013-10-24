5e8710f8412b5736d29f32c8a28da8391dc0b41d
kchapelier <kevin.chapelier@gmail.com>
2013-10-24 21:49:59 +0200

    Add music.tuning and do some doc

---
+++ src/aural.music.tuning.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Aural.Music.Tuning = {
+	middleA : 440,
+	defaultMiddleA : 440,
+	/**
+	 * Set the frequency of the middle A (A4)
+	 * @param (float) frequency - Frequency
+	 */
+	setMiddleA : function(frequency) {
+		frequency = frequency || this.defaultMiddleA;
+		this.middleA = frequency;
+	}
+};
