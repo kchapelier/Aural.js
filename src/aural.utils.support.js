c85b3e3e991016e5e05c7d38c1f1cd32f09580b5
kchapelier <kevin.chapelier@gmail.com>
2014-06-15 08:39:41 +0200

    add basic support of OfflineAudioContext in Aural.Utils.Support

--- src/aural.utils.support.js
+++ src/aural.utils.support.js
@@ -3,6 +3,8 @@
 Aural.Utils.Support = {};
 
 Aural.Utils.Support.audioContextClass = null;
+Aural.Utils.Support.offlineAudioContextClass = null;
+
 Aural.Utils.Support.audioContextCollection = {};
 
 if(!!window['AudioContext']) {
@@ -11,6 +13,14 @@ if(!!window['AudioContext']) {
 	Aural.Utils.Support.audioContextClass = window.webkitAudioContext;
 } else if(!!window['mozAudioContext']) {
 	Aural.Utils.Support.audioContextClass = window.mozAudioContext;
+}
+
+if(!!window['OfflineAudioContext']) {
+	Aural.Utils.Support.offlineAudioContextClass = window.OfflineAudioContext;
+} else if(!!window['webkitOfflineAudioContext']) {
+	Aural.Utils.Support.offlineAudioContextClass = window.webkitOfflineAudioContext;
+} else if(!!window['mozOfflineAudioContext']) {
+	Aural.Utils.Support.offlineAudioContextClass = window.mozOfflineAudioContext;
 }
 
 /**
@@ -44,11 +54,41 @@ Aural.Utils.Support.getAudioContext = function(identifier) {
 };
 
 /**
+ * Return whether the OfflineAudioContext is supported
+ * @return {boolean} Support
+ */
+Aural.Utils.Support.isOfflineAudioContextSupported = function() {
+	return (this.offlineAudioContextClass !== null);
+};
+
+/**
+ * Get a new instance of OfflineAudioContext
+ * @param {Number} channels Number of channels of the desired result of the OfflineAudioContext
+ * @param {Number} length Length of the desired result of the OfflineAudioContext
+ * @param {Number} [sampleRate] Samplerate
+ * @returns {*}
+ */
+Aural.Utils.Support.getOfflineAudioContext = function(channels, length, sampleRate) {
+	var context = null;
+
+	if(this.isOfflineAudioContextSupported()) {
+		channels = Math.min(1, parseInt(channels, 10));
+		sampleRate = sampleRate || this.getSampleRate();
+		context = new this.offlineAudioContextClass(channels, length, sampleRate);
+	}
+
+	return context;
+};
+
+/**
  * Return whether the ArrayBuffer and the TypedArrays are supported
  * @return {boolean} Support
  */
 Aural.Utils.Support.isArrayBufferAndTypedArraySupported = function() {
-	var classes = ['ArrayBuffer', 'Uint8Array', 'Int8Array', 'Uint16Array', 'Int16Array', 'Uint32Array', 'Int32Array', 'Float32Array'];
+	var classes = [
+		'ArrayBuffer', 'Uint8Array', 'Int8Array', 'Uint16Array', 'Int16Array',
+		'Uint32Array', 'Int32Array', 'Float16Array', 'Float32Array'
+	];
 	var supported = true;
 
 	for(var l = classes.length; l-- && supported;) {
@@ -60,7 +100,7 @@ Aural.Utils.Support.isArrayBufferAndTypedArraySupported = function() {
 
 /**
  * Return the sample rate
- * @returns {number| Sample rate
+ * @returns {number} Sample rate
  */
 Aural.Utils.Support.getSampleRate = function() {
 	return this.getAudioContext().sampleRate;
